78f16b015f47af47f02cf974c051b451511df4a9
Varun <varundpatil.04@gmail.com>
2025-11-19 19:33:48 +0530

    New Files

---
+++ 16_while_loop.py
@@ -0,0 +1,13 @@
+# while loop 
+i = 1
+
+while(i<6):
+    print(i)
+    i += 1
+
+l = [1, "varun", False, True, 5, 4]
+i = 0
+
+while(i<len(l)):
+    print(l[i])
+    i += 1
---
+++ 18_for_with_else.py
@@ -0,0 +1,8 @@
+l = [1, 4, 5]
+
+for i in l:
+    print(i)
+
+else:
+    print("Done")
+
